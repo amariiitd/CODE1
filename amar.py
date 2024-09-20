129c787295092ed32a52375d45c65cb37a6a446b
AMAR ANAND <162542674+amariiitd@users.noreply.github.com>
2024-09-20 17:50:21 +0530

    Create amar.py

---
+++ amar.py
@@ -0,0 +1,10 @@
+
+rows = 10
+cols = 10
+for i in range(0, rows):
+    for j in range(0, cols):
+        if i==0 or j==0 or i == rows-1 or j == cols-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
